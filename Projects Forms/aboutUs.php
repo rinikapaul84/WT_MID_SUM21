3d9fa753d8fda080105db227d498c6cdf03826e4
rinika <rinikapaul96@gmail.com>
2021-06-26 01:25:28 +0600

    forms converted to php files

---
+++ Projects Forms/aboutUs.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>Business Essential Course Details</title>
+</head>
+<body>
+    <h4><center>Learn Panda</center></h4>
+    <p>Learn Panda is where smart and enthusiastic Bangladeshi people, who are lifelong learners, come to get the skills they need and to transform their lives as they want. We are a community of enthusiastic learners united in a shared objective of life transformation through self-learning. Learn Panda is Bangladesh’s leading educational platform for superior online courses, professional training and corporate eLearning services that help you develop useful skills and accomplish more in life. Our online courses and career-track programs are designed in collaboration with the best of industry experts and organizations in Bangladesh.</p>
+
+    <h2><center>Our Mission</center></h2>
+    <p>Our mission is to even up education through the offering of high-quality learning opportunities that are accessible, flexible, and affordable. Virtually anyone anywhere in Bangladesh with an internet connection and a determination of self-learning can come to Bohubrihi, master an array of skills, and start working to pursue his dreams.</p>
+    
+    <h2><center>Our Vision</center></h2>
+    <p>Learn Panda is where motivated learners will come to learn the skills they need, to pursue the jobs they wish, to build things they want and to lead the lives they deserve. Students will be offered with opportunity to do some close to real-world projects curated by experts. These will help them to have the experience and portfolio for future. The online courses will come from not only individuals but rather renowned and established organizations of many different sectors- to provide the best quality education and recognition to the people of Bangladesh. </p>
+    
+    <h2><center>What's In The Future?</center></h2>
+    <p>Our future learning models and personalized support system will enable an unimaginable degree of engagement with our students and instructors. And, we will be with them through every phase of their learning graph: from the time the social media team may reply a query on Facebook Page, to the fulfilling moment when the career-counselling team will get the email that a learner has found a job or started something new with the knowledge he gained here. Our priority is always the betterment of our students, and this flare will always inspire us to offer high-quality practical education to as many people as we can reach through internet. </p>
+   
+    </body>
+</html>
